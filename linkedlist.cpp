740853ee7411cfae42acc64900f2e55d1d7009d2
Priyatosh11 <119726190+Priyatosh11@users.noreply.github.com>
2023-08-06 04:42:22 +0530

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,42 @@
+ #include <bits/stdc++.h> 
+#include<stack>
+bool findRedundantBrackets(string &s)
+{
+    // Write your code here.
+    stack<char> st;
+    for(int i=0; i<s.length(); i++){
+        char ch=s[i];
+
+        if(ch=='('|| ch=='+'|| ch=='-' || ch=='/'|| ch=='*')
+        {
+            st.push(ch);
+        }
+        else
+        {
+
+            //ch ya toh ')' or lowercase letter
+            if(ch==')')
+            {
+                bool isReduntant=true;
+
+                while(st.top()!='(')
+                {
+                    char top= st.top();
+                    if(top=='+'|| top=='-' || top=='/'|| top=='*')
+                    {
+                        isReduntant=false;
+
+                    }
+                     st.pop();
+                }
+
+                if(isReduntant==true)
+                    return true;
+                    st.pop();
+            }
+        }
+
+    }
+            return false;
+
+}
